bb9f641cc59c597cf3e723509ecf92de0c53ea93
ZorTsou <zortsou@gmail.com>
2017-07-03 10:18:43 +0800

    change spill Threshold.

--- hcat-server/src/main/scala/org/apache/spark/util/collection/Spillable.scala
+++ hcat-server/src/main/scala/org/apache/spark/util/collection/Spillable.scala
@@ -74,7 +74,7 @@ private[spark] trait Spillable[C] extends Logging {
     * @return true if `collection` was spilled to disk; false otherwise
     */
   protected def maybeSpill(collection: C, currentMemory: Long): Boolean = {
-    var shouldSpill = currentMemory >= 1024l * 1024l * 1024l * 3l;
+    var shouldSpill = currentMemory >= 1024l * 1024l * 1024l * 4l;
     /*
     var shouldSpill = false
     if (elementsRead % 32 == 0 && currentMemory >= myMemoryThreshold) {
