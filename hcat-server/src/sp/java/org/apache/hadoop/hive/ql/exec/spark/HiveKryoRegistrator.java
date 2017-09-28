cb2f5f86b24c5562e692a89f22daab028f63f332
ZorTsou <zortsou@gmail.com>
2017-09-28 10:50:03 +0800

    HIVE-15104: Hive on Spark generate more shuffle data than hive on mr. notice: HiveKey.class and HiveKryoRegistrator.class need add into spark-assembly-xxx.jar.

---
+++ hcat-server/src/sp/java/org/apache/hadoop/hive/ql/exec/spark/HiveKryoRegistrator.java
@@ -0,0 +1,56 @@
+package org.apache.hadoop.hive.ql.exec.spark;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.apache.hadoop.hive.ql.io.HiveKey;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.spark.serializer.KryoRegistrator;
+
+import java.util.Arrays;
+
+/**
+ * see HIVE-15104
+ * Add this class and HiveKey.class into spark-assembly-xx.jar
+ * Created by zrc on 17-9-18.
+ */
+public class HiveKryoRegistrator implements KryoRegistrator {
+
+    @Override
+    public void registerClasses(Kryo kryo) {
+        kryo.register(HiveKey.class, new HiveKeySerializer());
+        kryo.register(BytesWritable.class, new BytesWritableSerializer());
+    }
+
+    private static class HiveKeySerializer extends Serializer<HiveKey> {
+
+        @Override
+        public void write(Kryo kryo, Output output, HiveKey object) {
+            output.writeInt(object.getLength(), true);
+            output.write(object.getBytes(), 0, object.getLength());
+        }
+
+        @Override
+        public HiveKey read(Kryo kryo, Input input, Class<HiveKey> type) {
+            int len = input.readInt(true);
+            byte[] bytes = new byte[len];
+            input.readBytes(bytes);
+            return new HiveKey(bytes, Arrays.hashCode(bytes));
+        }
+    }
+
+    private static class BytesWritableSerializer extends Serializer<BytesWritable> {
+        public void write(Kryo kryo, Output output, BytesWritable object) {
+            output.writeInt(object.getLength(), true);
+            output.write(object.getBytes(), 0, object.getLength());
+        }
+
+        public BytesWritable read(Kryo kryo, Input input, Class<BytesWritable> type) {
+            int len = input.readInt(true);
+            byte[] bytes = new byte[len];
+            input.readBytes(bytes);
+            return new BytesWritable(bytes);
+        }
+    }
+}
